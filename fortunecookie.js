e6d2eeec4498fa4d43753750c3a0500164353237
julian <julianrymar@gmail.com>
2018-10-02 20:25:11 -0500

    Initial Dump

---
+++ fortunecookie.js
@@ -0,0 +1,167 @@
+function fortuneCookie() {
+    var randomCookieNumber = Math.floor(Math.random()*54);
+    switch(randomCookieNumber) {
+        case 0:
+            alert("The fortune you seek is in another cookie.");
+            break;
+        case 1:
+            alert("A closed mouth gathers no feet.");
+            break;
+        case 2:
+            alert("A conclusion is simply the place where you got tired of thinking.");
+            break;
+        case 3:
+            alert("A cynic is only a frustrated optimist.");
+            break;
+        case 4:
+            alert("A foolish man listens to his heart. A wise man listens to cookies.");
+            break;
+        case 5:
+            alert("You will die alone and poorly dressed.");
+            break;
+        case 6:
+            alert("A fanatic is one who can't change his mind, and won't change the subject.");
+            break;
+        case 7:
+            alert("If you look back, you'll soon be going that way.");
+            break;
+        case 8:
+            alert("You will live long enough to open many fortune cookies.");
+            break;
+        case 9:
+            alert("An alien of some sort will be appearing to you shortly.");
+            break;
+        case 10:
+            alert("Do not mistake temptation for opportunity.");
+            break;
+        case 12:
+            alert("Flattery will go far tonight.");
+            break;
+        case 13:
+            alert("He who laughs at himself never runs out of things to laugh at.");
+            break;
+        case 14:
+            alert("He who laughs last is laughing at you.");
+            break;
+        case 15:
+            alert("He who throws dirt is losing ground.");
+            break;
+        case 16:
+            alert("Some men dream of fortunes, others dream of cookies.");
+            break;
+        case 17:
+            alert("The greatest danger could be your stupidity.");
+            break;
+        case 18:
+            alert("We don't know the future, but here's a cookie.");
+            break;
+        case 19:
+            alert("The world may be your oyster, but it doesn't mean you'll get its pearl.");
+            break;
+        case 20:
+            alert("You will be hungry again in one hour.");
+            break;
+        case 21:
+            alert("The road to riches is paved with homework.");
+            break;
+        case 22:
+            alert("You can always find happiness at work on Friday.");
+            break;
+        case 23:
+            alert("Actions speak louder than fortune cookies.");
+            break;
+        case 24:
+            alert("Because of your melodic nature, the moonlight never misses an appointment.");
+            break;
+        case 25:
+            alert("Don't behave with cold manners.");
+            break;
+        case 26:
+            alert("Don't forget you are always on our minds.");
+            break;
+        case 27:
+            alert("Fortune not found? Abort, Retry, Ignore.");
+            break;
+        case 28:
+            alert("Help! I am being held prisoner in a fortune cookie factory.");
+            break;
+        case 29:
+            alert("It's about time I got out of that cookie.");
+            break;
+        case 30:
+            alert("Never forget a friend. Especially if he owes you.");
+            break;
+        case 31:
+            alert("Never wear your best pants when you go to fight for freedom.");
+            break;
+        case 32:
+            alert("Only listen to the fortune cookie; disregard all other fortune telling units.");
+            break;
+        case 33:
+            alert("It is a good day to have a good day.");
+            break;
+        case 34:
+            alert("All fortunes are wrong except this one.");
+            break;
+        case 35:
+            alert("Someone will invite you to a Karaoke party.");
+            break;
+        case 36:
+            alert("That wasn't chicken.");
+            break;
+        case 37:
+            alert("There is no mistake so great as that of being always right.");
+            break;
+        case 38:
+            alert("You love Chinese food.");
+            break;
+        case 39:
+            alert("I am worth a fortune.");
+            break;
+        case 40:
+            alert("No snowflake feels responsible in an avalanche.");
+            break;
+        case 41:
+            alert("You will receive a fortune cookie.");
+            break;
+        case 42:
+            alert("Some fortune cookies contain no fortune.");
+            break;
+        case 43:
+            alert("Don't let statistics do a number on you.");
+            break;
+        case 44:
+            alert("You are not illiterate.");
+            break;
+        case 45:
+            alert("May you someday be carbon neutral.");
+            break;
+        case 46:
+            alert("You have rice in your teeth.");
+            break;
+        case 47:
+            alert("Avoid taking unnecessary gambles. Lucky numbers: 12, 15, 23, 28, 37");
+            break;
+        case 48:
+            alert("Ask your mom instead of a cookie.");
+            break;
+        case 49:
+            alert("This cookie contains 117 calories.");
+            break;
+        case 50:
+            alert("Hard work pays off in the future. Laziness pays off now.");
+            break;
+        case 51:
+            alert("You think it's a secret, but they know.");
+            break;
+        case 52:
+            alert("If a turtle doesn't have a shell, is it naked or homeless?");
+            break;
+        case 53:
+            alert("Change is inevitable, except for vending machines.");
+            break;
+        case 54:
+            alert("Don't eat the paper.");
+            break;
+     }
+}
